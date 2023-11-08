1121c43c69b75434dc686de27db7d7779c3f8e7d
samndambuki <sam.ndambuki08@gmail.com>
2023-11-08 15:19:54 +0300

    making a POST request

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Iproduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   cart: Iproduct[] = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   add(product: Iproduct) {
     this.cart.push(product);
-    console.log(`product${product.name} added to cart`);
+    this.http.post('/api/cart', this.cart).subscribe(() => {
+      console.log(`product${product.name} added to cart`);
+    });
   }
 }
