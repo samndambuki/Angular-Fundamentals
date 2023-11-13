ca3e91564b2d233222e956afdd648972b6cca78a
samndambuki <sam.ndambuki08@gmail.com>
2023-11-13 13:26:29 +0300

    form controls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
 import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, title: "Home - Joe's Robot Shop" },
@@ -14,6 +15,7 @@ const routes: Routes = [
   },
   { path: 'cart', component: CartComponent, title: "Cart - Joe's Robot Shop" },
   { path: 'sign-in', component: SignInComponent },
+  { path: 'form-controls', component: TemplateFormControlsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
